7588e0f788cee263a36eb193ee9f0e8259c2c3a2
Alf-Lotsu <alfredsklotsu@yahoo.com>
2020-05-20 19:01:33 +0000

    added new .sql file

---
+++ ii.sql
@@ -0,0 +1,12 @@
+create database names;
+	use names;
+	show databases;
+create table surenames(
+first_name varchar(100),
+last_name varchar(100)
+age int
+);
+	insert into names(first_name,last_name,age)
+		values('Jack','Brewer'20),
+		('Katie','Cummings',26),
+		('Danny','Danforth',30);
